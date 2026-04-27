a99db2ac27c0155ca62a4f757fd23ca3f5e1265d
Mysteria0 <ado.donnelly@oppi.luovi.fi>
2026-04-27 09:19:29 +0300

    Fixed holdtime resetting to 0 after holding an input for 1 second

--- Scripts/Input Reader/input_reader.gd
+++ Scripts/Input Reader/input_reader.gd
@@ -3,18 +3,19 @@ extends Node2D
 var validactions = ["move_left","move_right","move_down","move_up","move_leftdown"]
 var Recentinputs = []
 var currentinput : StringName
-var holdtime : float
+var holdtime : int
 var memorybuffer : int
 
 func _process(_delta: float) -> void:
 	if currentinput:
 		if Input.is_action_pressed(currentinput):
 			holdtime += 1
-		else:
+		if Input.is_action_just_released(currentinput):
 			currentinput = ""
 			holdtime = 0
 	else:
 		holdtime += 1
+	holdtime = clamp(holdtime,0,99)
 	if currentinput == "":
 		$Recent_input.text = "Neutral " + str(holdtime)
 	else:
@@ -27,10 +28,11 @@ func _process(_delta: float) -> void:
 		
 		
 func _input(event: InputEvent) -> void:
-	for i in validactions:
-		if event.is_action(i,true):
-			currentinput = i
-			Recentinputs.append(currentinput)
-			if str(event) != currentinput:
-				holdtime = 0
-			break
+	if !event.is_action(currentinput,true):
+		for i in validactions:
+			if event.is_action(i,true):
+				currentinput = i
+				Recentinputs.append(currentinput)
+				if str(event) != currentinput:
+					holdtime = 0
+				break
