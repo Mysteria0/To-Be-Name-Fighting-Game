6376adcf68dee03166f72d0c7fe49dcc93dbaef0
Mysteria0 <ado.donnelly@oppi.luovi.fi>
2026-04-27 13:13:31 +0300

    Made the input reader more robust

--- Scripts/Input Reader/input_reader.gd
+++ Scripts/Input Reader/input_reader.gd
@@ -1,38 +1,55 @@
 extends Node2D
 
-var validactions = ["move_left","move_right","move_down","move_up","move_leftdown"]
-var Recentinputs = []
-var currentinput : StringName
+var validMotionInputs = ["move_left","move_right","move_down","move_up","move_leftdown"]
+var validAttackInputs = ["action_a","action_b","action_c"]
+var RecentMotionInputs = []
+var currentMotionInput = "Neutral"
+var currentAttackInput = "Nothing"
 var holdtime : int
 var memorybuffer : int
 
+
 func _process(_delta: float) -> void:
-	if currentinput:
-		if Input.is_action_pressed(currentinput):
+	handle_MotionInputs()
+	if currentMotionInput == "Neutral":
+		$Recent_input.text = str(RecentMotionInputs) ##"Neutral " + str(holdtime)
+	else:
+		$Recent_input.text = str(currentMotionInput) + " " + str(holdtime)
+		
+	remove_OldMotionInputs()
+	
+func handle_MotionInputs() -> void:
+	if currentMotionInput != "Neutral":
+		if Input.is_action_pressed(currentMotionInput):
 			holdtime += 1
-		if Input.is_action_just_released(currentinput):
-			currentinput = ""
-			holdtime = 0
+			RecentMotionInputs[-1][1] = clamp(holdtime,1,999)
+		if Input.is_action_just_released(currentMotionInput):
+			currentMotionInput = "Neutral"
+			holdtime = 1
 	else:
 		holdtime += 1
-	holdtime = clamp(holdtime,0,99)
-	if currentinput == "":
-		$Recent_input.text = "Neutral " + str(holdtime)
-	else:
-		$Recent_input.text = str(currentinput) + " " + str(holdtime)
-	if Recentinputs.size() > 1 and memorybuffer == 30:
-		Recentinputs.pop_front()
-		memorybuffer = 0
-	else:
-		memorybuffer += 1
-		
-		
+	holdtime = clamp(holdtime,1,999)
+	
+func remove_OldMotionInputs() -> void:
+	if !RecentMotionInputs.is_empty() and !Input.is_action_pressed(currentMotionInput):
+		if memorybuffer >= 30:
+			RecentMotionInputs.pop_front()
+			memorybuffer = 0
+		else:
+			memorybuffer += 1
+
+
 func _input(event: InputEvent) -> void:
-	if !event.is_action(currentinput,true):
-		for i in validactions:
+	if event and !event.is_action(currentMotionInput,true):
+		for i in validMotionInputs:
 			if event.is_action(i,true):
-				currentinput = i
-				Recentinputs.append(currentinput)
-				if str(event) != currentinput:
-					holdtime = 0
+				currentMotionInput = i
+				RecentMotionInputs.append([i,1])
+				if str(event) != currentMotionInput:
+					holdtime = 1
+				break
+
+		for b in validAttackInputs:
+			if event.is_action(b,true):
+				currentAttackInput = b
 				break
