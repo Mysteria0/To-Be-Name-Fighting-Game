4536a62c15c3cb29eb8e5db0378ba2174e8f463a
Autumn <ado.donnelly@oppi.luovi.fi>
2026-05-11 14:52:12 +0300

    added projectile for the current scope of the school-project half of the project

--- Scripts/State Machine/Tsurugi States/tsurugi_crouching.gd
+++ Scripts/State Machine/Tsurugi States/tsurugi_crouching.gd
@@ -8,7 +8,16 @@ extends State
 func enter() -> void:
 	super()
 	parent.velocity.x = 0
-
+	
+	
+func process_input(_event: InputEvent) -> State:
+	if %InputReader.currentMotionInput == 8 or %InputReader.currentMotionInput == 7 or %InputReader.currentMotionInput == 9:
+		StateMachine.Old_input = %InputReader.currentMotionInput
+		return jump_state
+	if %InputReader.currentMotionInput == 4 or  %InputReader.currentMotionInput == 6:
+		return move_state
+	return null
+	
 func process_physics(delta: float) -> State:
 	%MovementCode.Move_Character(5,delta)
 	return null
